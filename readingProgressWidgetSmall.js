281c6ea89b03fc3773985fecb87846265281a0e5
behringlukas <lukasbehringer.lb11@googlemail.com>
2023-01-19 00:33:51 +0100

    first working, unstyled version with manual bookId input

---
+++ readingProgressWidgetSmall.js
@@ -0,0 +1,161 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: magic;
+//creating widget
+const widget = new ListWidget();
+const darkmode = false;
+
+//user credentials
+let userMail = "PUT YOUR MAIL ADRESSE INSIDE THESE QUOTATIONS";
+let password = "PUT YOUR PASSWORD INSIDE THESE QUOTATIONS";
+
+//get bearer token for user from api
+const tokenResult = await getToken(userMail, password);
+const token = tokenResult.data.login["token"];
+const userId = tokenResult.data.login.profile["id"];
+
+//get data from api
+const result = await getData(userId, token);
+console.log(result);
+const coverUrl = result.data.booksByReadingStateAndProfile[0]["cover"];
+const coverRequest = new Request(coverUrl);
+const totalPages = result.data.readingProgresses[0]["capacity"];
+const currentPage = result.data.readingProgresses[0]["progress"]; //
+const progressPercentage = Math.round((currentPage / totalPages) * 100);
+const pagesLeft = totalPages - currentPage;
+
+//add content and set layout
+if (coverUrl !== "") {
+  const coverImg = await coverRequest.loadImage();
+  const cover = widget.addImage(coverImg);
+  cover.centerAlignImage();
+  const coverPlaceholder = widget.addText(pagesLeft.toString() + " pages left");
+  coverPlaceholder.textColor = Color.black();
+  widget.addSpacer();
+} else {
+  const coverPlaceholder = widget.addText(pagesLeft.toString() + " pages left");
+  coverPlaceholder.textColor = Color.black();
+  widget.addSpacer();
+}
+const title = widget.addText(
+  result.data.booksByReadingStateAndProfile[0]["title"]
+);
+title.centerAlignText();
+const author = widget.addText(
+  result.data.booksByReadingStateAndProfile[0].authors[0]["name"]
+);
+author.centerAlignText();
+
+//set background and text styles
+widget.backgroundColor = Color.white();
+title.font = Font.boldSystemFont(15);
+title.textColor = Color.black();
+author.font = Font.systemFont(11);
+author.textColor = Color.black();
+
+//api call to get bearer token for user
+async function getToken(email, password) {
+  const api = "https://literal.club/graphql";
+  const query = `mutation login(
+    $email: String! 
+    $password: String!) {
+    login(
+      email: $email
+      password: $password) {
+        token
+        email
+        languages
+        profile {
+          id
+          handle
+          name
+          image
+      }
+    }
+  }`;
+
+  const body = {
+    query: query,
+    variables: {
+      email: `${email}`,
+      password: `${password}`,
+    },
+  };
+
+  const headers = {
+    "content-type": "application/json",
+  };
+
+  try {
+    const req = new Request(api);
+    req.headers = headers;
+    req.body = JSON.stringify(body);
+    req.method = "post";
+    const result = await req.loadJSON();
+    return result;
+  } catch (e) {
+    console.log("Ohje");
+  }
+}
+
+//api call
+async function getData(profileId, token) {
+  const api = "https://literal.club/graphql/";
+  const query = `query booksByReadingStateAndProfile(
+ 	   $limit: Int!
+  	 $offset: Int!
+   	 $readingStatus: ReadingStatus!
+   	 $profileId: String!
+  	 $bookIds: [String!]! 	   
+		) {
+        booksByReadingStateAndProfile(
+            limit: $limit,
+            offset: $offset,
+            readingStatus: $readingStatus,
+            profileId: $profileId)
+      {
+    		id    
+				title
+        cover
+        authors{
+        	name 
+        }
+				}
+			readingProgresses(bookIds: $bookIds, active: true){
+				bookId
+				capacity
+				progress
+				unit
+				completed
+			} 
+    }`;
+
+  const body = {
+    query: query,
+    variables: {
+      limit: 1,
+      offset: 0,
+      profileId: `${profileId}`,
+      readingStatus: "IS_READING",
+      bookIds: ["ckpq118h62629161givab162d3a"],
+    },
+  };
+
+  const headers = {
+    authorization: "Bearer " + token,
+    "content-type": "application/json",
+  };
+  const req = new Request(api);
+  req.headers = headers;
+  req.body = JSON.stringify(body);
+  req.method = "post";
+  const result = await req.loadJSON();
+  return result;
+}
+
+//set output of this widget
+Script.setWidget(widget);
+Script.complete();
+
+//preview widget in Scriptable
+widget.presentSmall();
